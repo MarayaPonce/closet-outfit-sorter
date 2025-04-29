b8eb378021e7ecfb340139bf81b2d5051e48e62a
Babuhacker24 <blasanta.ieu2024@student.ie.edu>
2025-04-29 18:23:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 #UI: User interface: 
 import tkinter as tk
 
-# Main window
+# UI's Home page
 root = tk.Tk()
 root.title("closet")
 root.geometry("400x300")
 
 # Add a label
-label = tk.Label(root, text="Welcome to your Wardrobe!", font=("Helvetica", 16))
+label = tk.Label(root, text="Welcome to your Wardrobe!", font=("Archivo Black", 20))
 label.pack(pady=20)
 
-# Run the window
+# Run the window. This 
 root.mainloop()
+
+#Buttons: 
+def add_clothes():
+    print("Add clothes")
+
+def view_clothes():
+    print("Show all saved clothes")
+
+btn_add = tk.Button(root, text="Add Clothing Item", command=add_clothes)
+btn_add.pack(pady=10)
+
+btn_view = tk.Button(root, text="View Wardrobe", command=view_clothes)
+btn_view.pack(pady=10)
+
 
