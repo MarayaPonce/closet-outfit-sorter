8da36c92caf472843dcd10bef6a49b9a17757a45
Babuhacker24 <blasanta.ieu2024@student.ie.edu>
2025-05-06 13:44:40 +0000

    Resolve merge conflict

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import requests
 from datetime import datetime, timedelta
 from typing import List, Dict
+from wardrobe import Wardrobe
 
 # --- CONFIG ---
 API_KEY = '2a7866daf05f37f889e8d6f275122659'
 UNITS = 'metric'
 STYLE_OPTIONS = ["sporty", "formal / elegant", "day-to-day", "day event", "night event"]
+wardrobe = Wardrobe()
 
 # Initialize wardrobe in session state
 if "wardrobe_data" not in st.session_state:
