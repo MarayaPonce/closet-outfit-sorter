c3619616e675597fef2604e65b737b111f409185
Babuhacker24 <blasanta.ieu2024@student.ie.edu>
2025-05-06 13:59:59 +0000

    back to basics

--- prototype.py
+++ prototype.py
@@ -101,9 +101,6 @@ elif page == "View Wardrobe":
                 st.markdown(f"- Tags: `{', '.join(item['tags'])}`")
                 if item['brand']:
                     st.markdown(f"- Brand: `{item['brand']}`")
-                if st.button("🗑️ Delete", key=f"delete_{idx}"):
-                    st.session_state.wardrobe_data.pop(idx)
-                    st.experimental_rerun()
                 st.markdown("---")
 
 elif page == "Suggest Outfit":
