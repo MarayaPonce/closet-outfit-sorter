49d25c63611ee9d632b0288f899c2bf8d7b2b6b2
Babuhacker24 <blasanta.ieu2024@student.ie.edu>
2025-05-06 16:20:29 +0200

    add delete button

--- prototype.py
+++ prototype.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import requests
 from datetime import datetime, timedelta
@@ -101,6 +102,9 @@ elif page == "View Wardrobe":
                 st.markdown(f"- Tags: `{', '.join(item['tags'])}`")
                 if item['brand']:
                     st.markdown(f"- Brand: `{item['brand']}`")
+                if st.button("🗑️ Delete", key=f"delete_{idx}"):
+                    st.session_state.wardrobe_data.pop(idx)
+                    st.experimental_rerun()
                 st.markdown("---")
 
 elif page == "Suggest Outfit":
