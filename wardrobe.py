9a618285429b21d5130642bc79738dbc1baea2d3
Babuhacker24 <blasanta.ieu2024@student.ie.edu>
2025-05-06 17:45:52 +0200

    new csv file and changes in wardrobe.py (doc with pandas)

--- wardrobe.py
+++ wardrobe.py
@@ -2,9 +2,9 @@ import pandas as pd
 class Wardrobe:
     def __init__(self):
         # Read csv file into state
-        self.state = pd.read_csv("wardrobe.csv")
+        self.state = pd.read_csv("closet.csv")
     def add_item(self, item):
         self.state[item["name"]] = item["details"]
-        self.state.write_csv("wardrobe.csv")
+        self.state.write_csv("closet.csv")
     def get_items(self):
         return self.state.to_dict()
