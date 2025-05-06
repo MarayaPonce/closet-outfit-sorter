6d054453c290c2fc00e122ea7de899e3dfee3274
leirebarba <lbarba.ieu2024@student.ie.edu>
2025-05-06 18:23:58 +0200

    added init file and entered other files into innit

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from datetime import datetime, timedelta
 from typing import List, Dict
-from wardrobe import Wardrobe
+from src.wardrobe import Wardrobe
 
 # --- CONFIG ---
 API_KEY = '2a7866daf05f37f889e8d6f275122659'
