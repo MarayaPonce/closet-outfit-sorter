3ce60aa43eddfe8486cd2b78a18793246e695889
leirebarba <lbarba.ieu2024@student.ie.edu>
2025-05-08 10:19:24 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='closet-outfit-sorter',
+    version='0.1.0',  # Starting version
+    description='An online wardrobe where you can insert your clothes and get outfit suggestions based on occasion and weather.',
+
+    packages=find_packages("src"),  # Automatically finds all packages in your project
+
+)
